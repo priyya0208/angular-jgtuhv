2d67168b9d539c7c8e19c94fdf78b4f8d1ae227c
priyya0208 <priyya.0208@gmail.com>
2020-01-22 10:10:04 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ serverElements = [{type: 'server', name: 'testServer', content: 'This is just a server'}];
+ 
+
+   onServerAdded(serverData:{serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueAdded(bluePrintData:{serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: bluePrintData.serverName,
+      content: bluePrintData.serverContent
+    });
+  }
+}
